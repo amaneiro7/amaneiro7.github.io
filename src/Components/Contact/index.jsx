41044a547687ac9a1c9505e0304459a909b0f809
Andres Maneiro <amaneiro7@gmail.com>
2023-02-04 18:32:02 -0400

    [ADD] contact section

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,25 +1,75 @@
 import React from 'react';
+import {useForm, ValidationError} from '@formspree/react'
 import './Contact.css'
 
 export function Contact() {
+    const [ state, handleSubmit] = useForm("mwkjvdyb")
+    if (state.succeeded) {
+        return <p>
+            Thanks for contact me!
+        </p>
+    }
     return (
         <section id='contact'>
             <h3>CONTACT</h3>
             <hr />
             <div>
-                <p></p>
-                <input type="email" />
-                <p></p>
-                <input type="text" />
-                <button type='button'>
-                    Send
-                </button>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="email">
+                        Email Address:
+                    </label>
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                    />
+                    <ValidationError
+                        prefix="Email"
+                        field="email"
+                        errors={state.errors}
+                    />
+                    <label htmlFor="">
+                        Your message:
+                    </label>
+                    <textarea
+                        id="message"
+                        name="message"
+                    />
+                    <ValidationError
+                        prefix="Message"
+                        field="message"
+                        errors={state.errors}
+                    />
+                    <button 
+                        type="submit" 
+                        disabled={state.submitting}
+                        onClick={()=> onHandleclik}
+                    >
+                            Send
+                    </button>
+                </form>
             </div>
             <ul>
-                <li><a href="/"></a>LinkedIn</li>
-                <li><a href="/"></a>GitHub</li>
-                <li><a href="/"></a>Twitter</li>
-                <li><a href="/"></a>Instagram</li>
+                <li>
+                    <a href="https://www.linkedin.com/in/amaneiro7/">
+                        <img src="../../assets/images/linkedin-round.svg" alt="linkdIn" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/amaneiro7">
+                        <img src="../../assets/images/github.svg" alt="GitHub" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/Andruelectronic">
+                        <img src="../../assets/images/twitter.svg" alt="Twitter" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/amaneiro7/">
+                        <img src="../../assets/images/instagram.svg" alt="Instagram" />
+                    </a>
+                </li>
             </ul>
         </section>
     )
