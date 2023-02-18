a765e9986dc7205b2c631b65e9a224885d21e32e
Andres Maneiro <amaneiro7@gmail.com>
2023-02-18 01:03:24 -0400

    [fix] image error

--- build/main.f1f0c415152a1d1f82ee.js
+++ build/main.f1f0c415152a1d1f82ee.js
@@ -3423,7 +3423,7 @@ var portfolioDB = [{
   deploy_link: " https://amaneiro7.github.io/404PageChallenge/",
   repository_link: "https://github.com/amaneiro7/404PageChallenge"
 }, {
-  name: "Platzi Blog React-Dom",
+  name: "Platzi Blog React-Routers-Dom",
   description: "This is a project from a challenge from platzi, to put in practice React-Dom, privite routes, and a fake login",
   image: "../assets/portfolio/404pagechallenge.jpg",
   deploy_link: " https://amaneiro7.github.io/react-dom-platzi-pratice/",
